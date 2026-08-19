d2dd7941de60eec55f4161215c6ac3e1cb4b3fc6
Juan <juanlouw209@gmail.com>
2026-08-19 13:01:27 +0200

    Use GetConnectionString for DB context config
    Updated Program.cs to use builder.Configuration.GetConnectionString("DefaultConnection") instead of direct indexer access for retrieving the SQL Server connection string, improving clarity and leveraging the built-in configuration method.

--- StudentResultApp/Program.cs
+++ StudentResultApp/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddRazorComponents()
 builder.Services.AddScoped<ModuleService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration["DefaultConnection"]));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
